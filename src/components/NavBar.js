5449e25df87d3ca6fab1b9ea21749d6f9f366a27
Noel Kirkland <noelkirkland@gmail.com>
2020-10-06 14:42:04 -0700

    complete styling of NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBarHeaderText = styled.h1`
-font-size: 80px;
-text-align: center;
-color: #39ff14;
-background-color: black;
-`;
-
-const NavBarButtons = styled.li`
-display: inline;
-padding: 1%;
+const Border = styled.div`
+border: 1px solid #000000;
+margin: 5px
 `;
 
 function NavBar(){
   return(
-    <React.Fragment>
-      <NavBarHeaderText>Hack Your Health!</NavBarHeaderText>
-      <ul>
-        <NavBarButtons>
-          <Link to='/'>Home</Link>
-        </NavBarButtons>
-        <NavBarButtons>
-          <Link to='/signin'>Sign In</Link>
-        </NavBarButtons>
-      </ul>
-    </React.Fragment>
+    <Border>
+      <Navbar bg="light">
+        <Link to='/'>
+          <Navbar.Brand>Hack Your Health</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <a href="/signup">Sign up   |</a>
+          </Navbar.Text>
+          <Navbar.Text>
+            <a href="/signin">|   Sign in</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </Border>
   )
 }
 
